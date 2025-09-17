a0fd442d57ec32da2d1616b6c7ea768f06d321a3
Adam Smelko <adamsmelko1@gmail.com>
2025-09-17 14:11:32 +0000

    Update devcontainer wrt vcpkg

--- .devcontainer/configure.sh
+++ .devcontainer/configure.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install LLVM
+sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+
+# Install vcpkg
 cd /usr/local
 sudo git clone https://github.com/microsoft/vcpkg.git
 sudo chown -R $USER /usr/local/vcpkg
