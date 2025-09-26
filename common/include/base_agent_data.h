bbca2576d8e29755e0150b97b5917648b8d6ad81
Adam Smelko <adamsmelko1@gmail.com>
2025-09-26 06:54:41 +0000

    Make agent_data and base_agent_data generic classes

--- common/include/base_agent_data.h
+++ common/include/base_agent_data.h
@@ -1,20 +1,55 @@
 #pragma once
 
+#include <cassert>
+#include <concepts>
 #include <cstring>
 #include <vector>
 
 #include "types.h"
 
 namespace physicore {
-struct base_agent_data
+
+template <typename T, typename ValueType>
+concept VectorLike = requires(T t, ValueType val, std::size_t n) {
+	typename T::value_type;
+	{ t.push_back(val) };
+	{ t.size() } -> std::convertible_to<std::size_t>;
+	{ t[n] } -> std::convertible_to<ValueType&>;
+	{ t.resize(n) };
+};
+
+template <template <typename...> typename ContainerType = std::vector>
+struct base_agent_data_generic
 {
+	static_assert(VectorLike<ContainerType<real_t>, real_t>,
+				  "ContainerType must satisfy VectorLike concept with real_t");
+
 	index_t agents_count = 0;
 	index_t dims;
 
-	explicit base_agent_data(index_t dims = 3);
+	explicit base_agent_data_generic(index_t dims = 3) : dims(dims) {}
 
-	void add();
-	void remove_at(index_t position);
+	void add()
+	{
+		++agents_count;
+		positions.resize(agents_count * dims);
+	}
+
+	void remove_at(index_t position)
+	{
+		assert(position < agents_count);
+
+		if (position >= agents_count)
+			return;
+		--agents_count;
+
+		if (position != agents_count)
+		{
+			move_vector(&positions[position * dims], &positions[agents_count * dims], dims);
+		}
+
+		positions.resize(agents_count * dims);
+	}
 
 	template <typename T>
 	static void move_scalar(T* dst, const T* src)
@@ -28,6 +63,9 @@ struct base_agent_data
 		std::memcpy(dst, src, size * sizeof(T));
 	}
 
-	std::vector<real_t> positions;
+	ContainerType<real_t> positions;
 };
+
+using base_agent_data = base_agent_data_generic<std::vector>;
+
 } // namespace physicore
--- reactions-diffusion/biofvm/include/agent_data.h
+++ reactions-diffusion/biofvm/include/agent_data.h
@@ -7,31 +7,105 @@
 
 namespace physicore::biofvm {
 
-struct agent_data
+template <template <typename...> typename ContainerType = std::vector>
+struct agent_data_generic
 {
 public:
-	physicore::base_agent_data& base_data;
+	physicore::base_agent_data_generic<ContainerType>& base_data;
 
 	// n * substrate_count
-	std::vector<real_t> secretion_rates;
-	std::vector<real_t> saturation_densities;
-	std::vector<real_t> uptake_rates;
-	std::vector<real_t> net_export_rates;
+	ContainerType<real_t> secretion_rates;
+	ContainerType<real_t> saturation_densities;
+	ContainerType<real_t> uptake_rates;
+	ContainerType<real_t> net_export_rates;
 
 	// n * substrate_count
-	std::vector<real_t> internalized_substrates;
-	std::vector<real_t> fraction_released_at_death;
-	std::vector<real_t> fraction_transferred_when_ingested;
+	ContainerType<real_t> internalized_substrates;
+	ContainerType<real_t> fraction_released_at_death;
+	ContainerType<real_t> fraction_transferred_when_ingested;
 
 	// n
-	std::vector<real_t> volumes;
+	ContainerType<real_t> volumes;
 
 	index_t substrate_count;
 
-	explicit agent_data(physicore::base_agent_data& base_data, index_t substrate_count = 1);
+	explicit agent_data_generic(physicore::base_agent_data_generic<ContainerType>& base_data,
+								index_t substrate_count = 1);
 
 	void add();
 	void remove_at(index_t position);
 };
+
+using agent_data = agent_data_generic<std::vector>;
+
+template <template <typename...> typename ContainerType>
+agent_data_generic<ContainerType>::agent_data_generic(physicore::base_agent_data_generic<ContainerType>& base_data,
+													  index_t substrate_count)
+	: base_data(base_data), substrate_count(substrate_count)
+{}
+
+template <template <typename...> typename ContainerType>
+void agent_data_generic<ContainerType>::add()
+{
+	base_data.add();
+
+	secretion_rates.resize(base_data.agents_count * substrate_count);
+	saturation_densities.resize(base_data.agents_count * substrate_count);
+	uptake_rates.resize(base_data.agents_count * substrate_count);
+	net_export_rates.resize(base_data.agents_count * substrate_count);
+
+	internalized_substrates.resize(base_data.agents_count * substrate_count);
+	fraction_released_at_death.resize(base_data.agents_count * substrate_count);
+	fraction_transferred_when_ingested.resize(base_data.agents_count * substrate_count);
+
+	volumes.resize(base_data.agents_count);
+}
+
+template <template <typename...> typename ContainerType>
+void agent_data_generic<ContainerType>::remove_at(index_t position)
+{
+	assert(position < base_data.agents_count);
+
+	if (position >= base_data.agents_count)
+		return;
+
+	base_data.remove_at(position);
+
+	if (position < base_data.agents_count)
+	{
+		base_agent_data::move_vector(&secretion_rates[position * substrate_count],
+									 &secretion_rates[base_data.agents_count * substrate_count], substrate_count);
+		base_agent_data::move_vector(&saturation_densities[position * substrate_count],
+									 &saturation_densities[base_data.agents_count * substrate_count], substrate_count);
+		base_agent_data::move_vector(&uptake_rates[position * substrate_count],
+									 &uptake_rates[base_data.agents_count * substrate_count], substrate_count);
+		base_agent_data::move_vector(&net_export_rates[position * substrate_count],
+									 &net_export_rates[base_data.agents_count * substrate_count], substrate_count);
+
+		base_agent_data::move_vector(&internalized_substrates[position * substrate_count],
+									 &internalized_substrates[base_data.agents_count * substrate_count],
+									 substrate_count);
+		base_agent_data::move_vector(&fraction_released_at_death[position * substrate_count],
+									 &fraction_released_at_death[base_data.agents_count * substrate_count],
+									 substrate_count);
+		base_agent_data::move_vector(&fraction_transferred_when_ingested[position * substrate_count],
+									 &fraction_transferred_when_ingested[base_data.agents_count * substrate_count],
+									 substrate_count);
+
+		base_agent_data::move_scalar(&volumes[position], &volumes[base_data.agents_count]);
+	}
+
+	secretion_rates.resize(base_data.agents_count * substrate_count);
+	saturation_densities.resize(base_data.agents_count * substrate_count);
+	uptake_rates.resize(base_data.agents_count * substrate_count);
+	net_export_rates.resize(base_data.agents_count * substrate_count);
+
+	internalized_substrates.resize(base_data.agents_count * substrate_count);
+	fraction_released_at_death.resize(base_data.agents_count * substrate_count);
+	fraction_transferred_when_ingested.resize(base_data.agents_count * substrate_count);
+
+	volumes.resize(base_data.agents_count);
+}
+
 
 } // namespace physicore::biofvm
