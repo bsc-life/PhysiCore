042b78ea8c6f709ab48892c1bf14a64bddf23e9e
Adam Smelko <adamsmelko1@gmail.com>
2025-09-29 06:24:42 +0000

    Make agent a generic class

--- reactions-diffusion/biofvm/include/agent.h
+++ reactions-diffusion/biofvm/include/agent.h
@@ -6,24 +6,54 @@
 
 namespace physicore::biofvm {
 
-class agent : public physicore::base_agent
+template <typename AgentDataType>
+class agent_generic : public physicore::base_agent
 {
 protected:
-	agent_data& data;
+	AgentDataType& data;
 
 public:
-	agent(index_t index, agent_data& data);
+	agent_generic(index_t index, AgentDataType& data) : physicore::base_agent(index, data.base_data), data(data) {}
 
-	std::span<real_t> secretion_rates();
-	std::span<real_t> saturation_densities();
-	std::span<real_t> uptake_rates();
-	std::span<real_t> net_export_rates();
+	std::span<real_t> secretion_rates()
+	{
+		return std::span<real_t>(&data.secretion_rates[index * data.substrate_count], data.substrate_count);
+	}
 
-	std::span<real_t> internalized_substrates();
-	std::span<real_t> fraction_released_at_death();
-	std::span<real_t> fraction_transferred_when_ingested();
+	std::span<real_t> saturation_densities()
+	{
+		return std::span<real_t>(&data.saturation_densities[index * data.substrate_count], data.substrate_count);
+	}
 
-	real_t& volume();
+	std::span<real_t> uptake_rates()
+	{
+		return std::span<real_t>(&data.uptake_rates[index * data.substrate_count], data.substrate_count);
+	}
+
+	std::span<real_t> net_export_rates()
+	{
+		return std::span<real_t>(&data.net_export_rates[index * data.substrate_count], data.substrate_count);
+	}
+
+	std::span<real_t> internalized_substrates()
+	{
+		return std::span<real_t>(&data.internalized_substrates[index * data.substrate_count], data.substrate_count);
+	}
+
+	std::span<real_t> fraction_released_at_death()
+	{
+		return std::span<real_t>(&data.fraction_released_at_death[index * data.substrate_count], data.substrate_count);
+	}
+
+	std::span<real_t> fraction_transferred_when_ingested()
+	{
+		return std::span<real_t>(&data.fraction_transferred_when_ingested[index * data.substrate_count],
+								 data.substrate_count);
+	}
+
+	real_t& volume() { return data.volumes[index]; }
 };
+
+using agent = agent_generic<agent_data>;
 
 } // namespace physicore::biofvm
