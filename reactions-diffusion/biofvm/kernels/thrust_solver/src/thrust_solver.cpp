db327c0fafc95e8ce47e9fa30ef740a7475f445a
Adam Smelko <adamsmelko1@gmail.com>
2025-10-02 11:55:12 +0000

    Add thrust kernel registration

---
+++ reactions-diffusion/biofvm/kernels/thrust_solver/src/thrust_solver.cpp
@@ -0,0 +1,31 @@
+#include "thrust_solver.h"
+
+using namespace physicore;
+using namespace physicore::biofvm::kernels::thrust_solver;
+
+void thrust_solver::solve(biofvm::microenvironment& m, index_t iterations)
+{
+	if (!initialized)
+	{
+		d_solver.initialize(m, 1);
+
+		dir_solver.initialize(m);
+
+		b_solver.initialize(m);
+
+		c_solver.initialize(m);
+
+		initialized = true;
+	}
+
+	for (index_t it = 0; it < iterations; it++)
+	{
+		d_solver.solve();
+
+		dir_solver.solve(m, d_solver);
+
+		b_solver.solve(m, d_solver);
+
+		c_solver.simulate_secretion_and_uptake(m, d_solver, it == 0);
+	}
+}
