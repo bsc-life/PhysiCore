36a5c20df3db3623390131f219614ee810ea6cc6
Adam Smelko <adamsmelko1@gmail.com>
2025-10-09 07:53:56 +0000

    Fix and update cccl portfile

--- ports-overlays/cccl/portfile.cmake
+++ ports-overlays/cccl/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_from_git(
   OUT_SOURCE_PATH SOURCE_PATH URL https://github.com/NVIDIA/cccl.git REF
-  9c40ed11560fa8ffd21abe4cdc8dc3ce875e48e3)
+  8c04b6539859932f5602e86d38314e4d87f96420)
 
 vcpkg_cmake_configure(
   SOURCE_PATH
@@ -18,8 +18,31 @@ vcpkg_cmake_configure(
   -DCUB_ENABLE_INSTALL_RULES=ON
   -DThrust_ENABLE_INSTALL_RULES=ON)
 
-vcpkg_cmake_build()
-
 vcpkg_cmake_install()
 
-# vcpkg_cmake_config_fixup()
+file(GLOB_RECURSE HEADER_SEARCH_FILES
+     "${CURRENT_PACKAGES_DIR}/lib/cmake/*/*-header-search.cmake")
+
+# CCCL expects that the cmake files will stay under lib/cmake/PORT, but we will
+# be moving them to share/PORT. So we need to change the relative paths set in
+# the header search files.
+foreach(_file IN LISTS HEADER_SEARCH_FILES)
+  file(READ "${_file}" _contents)
+  string(REPLACE "set(from_install_prefix \"../../../\")"
+                 "set(from_install_prefix \"../../\")" _contents "${_contents}")
+  file(WRITE "${_file}" "${_contents}")
+endforeach()
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/cccl PACKAGE_NAME cccl
+                         DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/thrust PACKAGE_NAME thrust
+                         DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/cub PACKAGE_NAME cub
+                         DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/libcudacxx PACKAGE_NAME
+                         libcudacxx DO_NOT_DELETE_PARENT_CONFIG_PATH)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/lib")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
