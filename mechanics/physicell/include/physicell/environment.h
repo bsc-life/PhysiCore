b02542e0bbbf62d03bfcb297efc1770b363a3fc7
Jose Estragues <joseestragues@gmail.com>
2025-12-02 13:30:35 +0000

    mechs orig included

---
+++ mechanics/physicell/include/physicell/environment.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <common/timestep_executor.h>
+#include <common/types.h>
+
+namespace physicore::mechanics::physicell {
+
+class environment : public timestep_executor
+{
+	real_t timestep;
+
+public:
+	explicit environment(real_t timestep) : timestep(timestep) {}
+
+	void run_single_timestep() override;
+
+	void serialize_state(real_t current_time) override;
+};
+
+} // namespace physicore::mechanics::physicell
