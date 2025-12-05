84a40aeb6773079d2b926fca0afd8353c99b5bf5
Adam Šmelko <adamsmelko1@gmail.com>
2025-12-05 13:02:35 +0100

    fix: make macos artifacts use gcc

---
+++ triplets-overlays/arm64-osx-gcc.cmake
@@ -0,0 +1,12 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_ARCHITECTURES arm64)
+
+set(VCPKG_C_COMPILER "/opt/homebrew/bin/gcc-15")
+set(VCPKG_CXX_COMPILER "/opt/homebrew/bin/g++-15")
+
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_C_COMPILER=${VCPKG_C_COMPILER}"
+                                  "-DCMAKE_CXX_COMPILER=${VCPKG_CXX_COMPILER}")
