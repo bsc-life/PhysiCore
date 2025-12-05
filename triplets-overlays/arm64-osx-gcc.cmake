b8d45b66bc12051ff2df0c3773553dc28b82cb1a
Adam Smelko <adamsmelko1@gmail.com>
2025-12-05 10:33:22 +0000

    fix: add CMake configure options for GCC compilers in arm64 and x64 triplet files

--- triplets-overlays/arm64-osx-gcc.cmake
+++ triplets-overlays/arm64-osx-gcc.cmake
@@ -7,3 +7,6 @@ set(VCPKG_OSX_ARCHITECTURES arm64)
 
 set(VCPKG_C_COMPILER "/opt/homebrew/bin/gcc-15")
 set(VCPKG_CXX_COMPILER "/opt/homebrew/bin/g++-15")
+
+set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_C_COMPILER=${VCPKG_C_COMPILER}"
+                                  "-DCMAKE_CXX_COMPILER=${VCPKG_CXX_COMPILER}")
