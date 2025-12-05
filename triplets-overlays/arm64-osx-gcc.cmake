9f109c521dd497cb9472481dc48dc3b71aa256c1
Adam Smelko <adamsmelko1@gmail.com>
2025-12-05 09:51:41 +0000

    fix: update macOS build presets to use GCC and add triplet configurations for osx arm64 and x64

---
+++ triplets-overlays/arm64-osx-gcc.cmake
@@ -0,0 +1,9 @@
+set(VCPKG_TARGET_ARCHITECTURE arm64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
+
+set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
+set(VCPKG_OSX_ARCHITECTURES arm64)
+
+set(VCPKG_C_COMPILER "/opt/homebrew/bin/gcc-15")
+set(VCPKG_CXX_COMPILER "/opt/homebrew/bin/g++-15")
