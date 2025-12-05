1abc80937e632f2e11d25a3b3ce35207971f11c6
Adam Smelko <adamsmelko1@gmail.com>
2025-12-05 11:09:24 +0000

    fix: remove tbb and cccl dependency when building on macos

--- triplets-overlays/arm64-osx-gcc.cmake
+++ triplets-overlays/arm64-osx-gcc.cmake
@@ -10,6 +10,3 @@ set(VCPKG_CXX_COMPILER "/opt/homebrew/bin/g++-15")
 
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS "-DCMAKE_C_COMPILER=${VCPKG_C_COMPILER}"
                                   "-DCMAKE_CXX_COMPILER=${VCPKG_CXX_COMPILER}")
-
-set(VCPKG_C_FLAGS "-fcf-protection=none")
-set(VCPKG_CXX_FLAGS "-fcf-protection=none")
