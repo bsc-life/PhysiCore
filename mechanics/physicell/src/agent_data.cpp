e75f9f512372a8c89306c8aca0111a432e5d460d
Jose L Estragués <93603623+joseestraguesbsc@users.noreply.github.com>
2026-01-12 16:05:39 +0100

    feat: add mechanics skeleton
    * mechs orig included
    
    * PR ready for issue #67
    
    * PR ready for Issue #69
    
    * PR Comments solved
    
    * Closes #70
    
    * Config reader restructure and Sonar integration
    
    * Comments solved and implements #72
    
    * Code formatted
    
    * fixing formal checks
    
    * Comments solved
    
    * core duplication and coverage
    
    * hope the last commit of the year
    
    * clang-tidy errors
    
    * formal checks and code duplication
    
    ---------
    
    Co-authored-by: Jose Estragues <joseestragues@gmail.com>

---
+++ mechanics/physicell/src/agent_data.cpp
@@ -0,0 +1,140 @@
+#include "physicell/agent_data.h"
+
+#include <algorithm>
+#include <cassert>
+#include <memory>
+
+#include "physicell/mechanical_agent.h"
+#include "physicell/mechanical_parameters.h"
+
+namespace physicore::mechanics::physicell {
+
+mechanical_agent_data::mechanical_agent_data(physicore::base_agent_data_generic_storage<std::vector>& base_data,
+											 index_t agent_types_count, index_t substrates_count)
+	: base_data(base_data), agent_types_count(agent_types_count), substrates_count(substrates_count)
+{}
+
+void mechanical_agent_data::add()
+{
+	// Sync with base class
+	agents_count = base_data.agents_count;
+
+	resize_storage();
+}
+
+void mechanical_agent_data::remove_at(index_t position)
+{
+	assert(position < agents_count);
+	if (position >= agents_count)
+		return;
+
+	using base_storage_t = physicore::base_agent_data_generic_storage<std::vector>;
+
+	const index_t last = agents_count - 1;
+
+	if (position != last)
+	{
+		base_storage_t::move_vector(&velocity[position * base_data.dims], &velocity[last * base_data.dims],
+									base_data.dims);
+		base_storage_t::move_vector(&previous_velocity[position * base_data.dims],
+									&previous_velocity[last * base_data.dims], base_data.dims);
+		base_storage_t::move_scalar(&radius[position], &radius[last]);
+
+		// Mechanics properties
+		base_storage_t::move_scalar(&mechanics_data.cell_cell_adhesion_strength[position],
+									&mechanics_data.cell_cell_adhesion_strength[last]);
+		base_storage_t::move_scalar(&mechanics_data.cell_BM_adhesion_strength[position],
+									&mechanics_data.cell_BM_adhesion_strength[last]);
+		base_storage_t::move_scalar(&mechanics_data.cell_cell_repulsion_strength[position],
+									&mechanics_data.cell_cell_repulsion_strength[last]);
+		base_storage_t::move_scalar(&mechanics_data.cell_BM_repulsion_strength[position],
+									&mechanics_data.cell_BM_repulsion_strength[last]);
+
+		base_storage_t::move_vector(&mechanics_data.cell_adhesion_affinities[position * agent_types_count],
+									&mechanics_data.cell_adhesion_affinities[last * agent_types_count],
+									agent_types_count);
+
+		base_storage_t::move_scalar(&mechanics_data.relative_maximum_adhesion_distance[position],
+									&mechanics_data.relative_maximum_adhesion_distance[last]);
+		base_storage_t::move_scalar(&mechanics_data.maximum_number_of_attachments[position],
+									&mechanics_data.maximum_number_of_attachments[last]);
+		base_storage_t::move_scalar(&mechanics_data.attachment_elastic_constant[position],
+									&mechanics_data.attachment_elastic_constant[last]);
+		base_storage_t::move_scalar(&mechanics_data.attachment_rate[position], &mechanics_data.attachment_rate[last]);
+		base_storage_t::move_scalar(&mechanics_data.detachment_rate[position], &mechanics_data.detachment_rate[last]);
+
+		// Motility properties
+		base_storage_t::move_scalar(&motility_data.is_motile[position], &motility_data.is_motile[last]);
+		base_storage_t::move_scalar(&motility_data.persistence_time[position], &motility_data.persistence_time[last]);
+		base_storage_t::move_scalar(&motility_data.migration_speed[position], &motility_data.migration_speed[last]);
+		base_storage_t::move_vector(&motility_data.migration_bias_direction[position * base_data.dims],
+									&motility_data.migration_bias_direction[last * base_data.dims], base_data.dims);
+		base_storage_t::move_scalar(&motility_data.migration_bias[position], &motility_data.migration_bias[last]);
+		base_storage_t::move_vector(&motility_data.motility_vector[position * base_data.dims],
+									&motility_data.motility_vector[last * base_data.dims], base_data.dims);
+		base_storage_t::move_scalar(&motility_data.restrict_to_2d[position], &motility_data.restrict_to_2d[last]);
+		base_storage_t::move_scalar(&motility_data.chemotaxis_index[position], &motility_data.chemotaxis_index[last]);
+		base_storage_t::move_scalar(&motility_data.chemotaxis_direction[position],
+									&motility_data.chemotaxis_direction[last]);
+		base_storage_t::move_vector(&motility_data.chemotactic_sensitivities[position * substrates_count],
+									&motility_data.chemotactic_sensitivities[last * substrates_count],
+									substrates_count);
+
+		// State properties
+		state_data.neighbors[position] = std::move(state_data.neighbors[last]);
+		state_data.springs[position] = std::move(state_data.springs[last]);
+		state_data.attached_cells[position] = std::move(state_data.attached_cells[last]);
+		base_storage_t::move_vector(&state_data.orientation[position * base_data.dims],
+									&state_data.orientation[last * base_data.dims], base_data.dims);
+		base_storage_t::move_scalar(&state_data.simple_pressure[position], &state_data.simple_pressure[last]);
+		base_storage_t::move_scalar(&state_data.agent_type_index[position], &state_data.agent_type_index[last]);
+		base_storage_t::move_scalar(&state_data.is_movable[position], &state_data.is_movable[last]);
+	}
+
+	// Sync with base class
+	agents_count = base_data.agents_count;
+
+	resize_storage();
+}
+
+void mechanical_agent_data::resize_storage()
+{
+	velocity.resize(agents_count * base_data.dims);
+	previous_velocity.resize(agents_count * base_data.dims);
+	radius.resize(agents_count);
+
+	// Mechanics properties
+	mechanics_data.cell_cell_adhesion_strength.resize(agents_count);
+	mechanics_data.cell_BM_adhesion_strength.resize(agents_count);
+	mechanics_data.cell_cell_repulsion_strength.resize(agents_count);
+	mechanics_data.cell_BM_repulsion_strength.resize(agents_count);
+	mechanics_data.cell_adhesion_affinities.resize(agents_count * agent_types_count);
+	mechanics_data.relative_maximum_adhesion_distance.resize(agents_count);
+	mechanics_data.maximum_number_of_attachments.resize(agents_count);
+	mechanics_data.attachment_elastic_constant.resize(agents_count);
+	mechanics_data.attachment_rate.resize(agents_count);
+	mechanics_data.detachment_rate.resize(agents_count);
+
+	// Motility properties
+	motility_data.is_motile.resize(agents_count);
+	motility_data.persistence_time.resize(agents_count);
+	motility_data.migration_speed.resize(agents_count);
+	motility_data.migration_bias_direction.resize(agents_count * base_data.dims);
+	motility_data.migration_bias.resize(agents_count);
+	motility_data.motility_vector.resize(agents_count * base_data.dims);
+	motility_data.restrict_to_2d.resize(agents_count);
+	motility_data.chemotaxis_index.resize(agents_count);
+	motility_data.chemotaxis_direction.resize(agents_count);
+	motility_data.chemotactic_sensitivities.resize(agents_count * substrates_count);
+
+	// State properties
+	state_data.neighbors.resize(agents_count);
+	state_data.springs.resize(agents_count);
+	state_data.attached_cells.resize(agents_count);
+	state_data.orientation.resize(agents_count * base_data.dims);
+	state_data.simple_pressure.resize(agents_count);
+	state_data.agent_type_index.resize(agents_count);
+	state_data.is_movable.resize(agents_count);
+}
+
+} // namespace physicore::mechanics::physicell
---
+++ mechanics/physicell/src/mechanical_agent.cpp
@@ -0,0 +1,127 @@
+#include "physicell/mechanical_agent.h"
+
+#include <tuple>
+
+namespace physicore::mechanics::physicell {
+
+mechanical_agent::mechanical_agent(index_t index, mechanical_agent_data& data)
+	: base_agent_interface(index),
+	  physicore::base_agent_generic_storage<base_agent_data>(index, data.base_data),
+	  data(data)
+{}
+
+mechanical_agent::mechanical_agent(
+	index_t index, std::tuple<std::unique_ptr<base_agent_data>, std::unique_ptr<mechanical_agent_data>>& datas)
+	: mechanical_agent(index, *std::get<std::unique_ptr<mechanical_agent_data>>(datas))
+{}
+
+std::span<real_t> mechanical_agent::velocity()
+{
+	const index_t dims = data.base_data.dims;
+	return { &data.velocity[this->index * dims], dims };
+}
+
+std::span<real_t> mechanical_agent::previous_velocity()
+{
+	const index_t dims = data.base_data.dims;
+	return { &data.previous_velocity[this->index * dims], dims };
+}
+
+real_t& mechanical_agent::radius() { return data.radius[this->index]; }
+
+real_t& mechanical_agent::cell_cell_adhesion_strength()
+{
+	return data.mechanics_data.cell_cell_adhesion_strength[this->index];
+}
+
+real_t& mechanical_agent::cell_BM_adhesion_strength()
+{
+	return data.mechanics_data.cell_BM_adhesion_strength[this->index];
+}
+
+real_t& mechanical_agent::cell_cell_repulsion_strength()
+{
+	return data.mechanics_data.cell_cell_repulsion_strength[this->index];
+}
+
+real_t& mechanical_agent::cell_BM_repulsion_strength()
+{
+	return data.mechanics_data.cell_BM_repulsion_strength[this->index];
+}
+
+std::span<real_t> mechanical_agent::cell_adhesion_affinities()
+{
+	return { &data.mechanics_data.cell_adhesion_affinities[this->index * data.agent_types_count],
+			 data.agent_types_count };
+}
+
+real_t& mechanical_agent::relative_maximum_adhesion_distance()
+{
+	return data.mechanics_data.relative_maximum_adhesion_distance[this->index];
+}
+
+index_t& mechanical_agent::maximum_number_of_attachments()
+{
+	return data.mechanics_data.maximum_number_of_attachments[this->index];
+}
+
+real_t& mechanical_agent::attachment_elastic_constant()
+{
+	return data.mechanics_data.attachment_elastic_constant[this->index];
+}
+
+real_t& mechanical_agent::attachment_rate() { return data.mechanics_data.attachment_rate[this->index]; }
+
+real_t& mechanical_agent::detachment_rate() { return data.mechanics_data.detachment_rate[this->index]; }
+
+std::uint8_t& mechanical_agent::is_motile() { return data.motility_data.is_motile[this->index]; }
+
+real_t& mechanical_agent::persistence_time() { return data.motility_data.persistence_time[this->index]; }
+
+real_t& mechanical_agent::migration_speed() { return data.motility_data.migration_speed[this->index]; }
+
+std::span<real_t> mechanical_agent::migration_bias_direction()
+{
+	const index_t dims = data.base_data.dims;
+	return { &data.motility_data.migration_bias_direction[this->index * dims], dims };
+}
+
+real_t& mechanical_agent::migration_bias() { return data.motility_data.migration_bias[this->index]; }
+
+std::span<real_t> mechanical_agent::motility_vector()
+{
+	const index_t dims = data.base_data.dims;
+	return { &data.motility_data.motility_vector[this->index * dims], dims };
+}
+
+std::uint8_t& mechanical_agent::restrict_to_2d() { return data.motility_data.restrict_to_2d[this->index]; }
+
+index_t& mechanical_agent::chemotaxis_index() { return data.motility_data.chemotaxis_index[this->index]; }
+
+index_t& mechanical_agent::chemotaxis_direction() { return data.motility_data.chemotaxis_direction[this->index]; }
+
+std::span<real_t> mechanical_agent::chemotactic_sensitivities()
+{
+	return { &data.motility_data.chemotactic_sensitivities[this->index * data.substrates_count],
+			 data.substrates_count };
+}
+
+std::span<index_t> mechanical_agent::neighbors() { return { data.state_data.neighbors[this->index] }; }
+
+std::span<index_t> mechanical_agent::springs() { return { data.state_data.springs[this->index] }; }
+
+std::span<index_t> mechanical_agent::attached_cells() { return { data.state_data.attached_cells[this->index] }; }
+
+std::span<real_t> mechanical_agent::orientation()
+{
+	const index_t dims = data.base_data.dims;
+	return { &data.state_data.orientation[this->index * dims], dims };
+}
+
+real_t& mechanical_agent::simple_pressure() { return data.state_data.simple_pressure[this->index]; }
+
+index_t& mechanical_agent::agent_type_index() { return data.state_data.agent_type_index[this->index]; }
+
+std::uint8_t& mechanical_agent::is_movable() { return data.state_data.is_movable[this->index]; }
+
+} // namespace physicore::mechanics::physicell
