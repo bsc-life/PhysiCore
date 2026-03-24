ac66569b661b20b188842912bdc5a44b2b7326e0
Adam Šmelko <adamsmelko1@gmail.com>
2026-03-24 08:53:02 +0100

    docs: add generic_agent_container documentation (#113)
    Common docs page now contains container documentation.
    Also added a test that showcases the future usage.
    
    Fixes #89

--- common/tests/test_base_agent_container.cpp
+++ common/tests/test_base_agent_container.cpp
@@ -1,6 +1,8 @@
 #include <gtest/gtest.h>
 
 #include "base_agent_container.h"
+#include "base_agent_data.h"
+#include "generic_agent_solver.h"
 
 using namespace physicore;
 
@@ -58,3 +60,153 @@ TEST_P(RemoveAgentTest, RemoveAgentsAndCheckPositions)
 }
 
 INSTANTIATE_TEST_SUITE_P(BaseAgentContainerTest, RemoveAgentTest, ::testing::Values(0, 1, 2));
+
+class diffusion_agent_data
+{
+public:
+	base_agent_data& data;
+
+	void add() {}
+
+	void remove_at(index_t /* position */) {}
+
+	diffusion_agent_data(base_agent_data& data) : data(data) {}
+};
+
+class diffusion_agent_interface : public virtual base_agent_interface
+{};
+
+class diffusion_agent : public base_agent, public virtual diffusion_agent_interface
+{
+	diffusion_agent_data& data;
+
+public:
+	using DataType = diffusion_agent_data;
+	using InterfaceType = diffusion_agent_interface;
+
+	diffusion_agent(index_t index,
+					std::tuple<std::unique_ptr<base_agent_data>, std::unique_ptr<diffusion_agent_data>>& datas)
+		: base_agent_interface(index), base_agent(index, *std::get<0>(datas)), data(*std::get<1>(datas))
+	{}
+};
+
+class mechanics_agent_data
+{
+public:
+	base_agent_data& data;
+
+	void add() {}
+
+	void remove_at(index_t /* position */) {}
+
+	mechanics_agent_data(base_agent_data& data) : data(data) {}
+};
+
+class mechanics_agent_interface : public virtual base_agent_interface
+{};
+
+class mechanics_agent : public base_agent, public virtual mechanics_agent_interface
+{
+	mechanics_agent_data& data;
+
+public:
+	using DataType = mechanics_agent_data;
+	using InterfaceType = mechanics_agent_interface;
+
+	mechanics_agent(index_t index,
+					std::tuple<std::unique_ptr<base_agent_data>, std::unique_ptr<mechanics_agent_data>>& datas)
+		: base_agent_interface(index), base_agent(index, *std::get<0>(datas)), data(*std::get<1>(datas))
+	{}
+};
+
+class big_agent_data
+{
+public:
+	base_agent_data& data1;
+	diffusion_agent_data& data2;
+	mechanics_agent_data& data3;
+
+	void add() {}
+
+	void remove_at(index_t /* position */) {}
+
+	big_agent_data(base_agent_data& data1, diffusion_agent_data& data2, mechanics_agent_data& data3)
+		: data1(data1), data2(data2), data3(data3)
+	{}
+};
+
+class big_agent_interface : public virtual base_agent_interface,
+							public virtual diffusion_agent_interface,
+							public virtual mechanics_agent_interface
+{};
+
+class big_agent : public base_agent, public virtual big_agent_interface
+{
+public:
+	big_agent_data& data;
+
+	using DataType = big_agent_data;
+	using InterfaceType = big_agent_interface;
+
+	big_agent(index_t index, std::tuple<std::unique_ptr<base_agent_data>, std::unique_ptr<diffusion_agent_data>,
+										std::unique_ptr<mechanics_agent_data>, std::unique_ptr<big_agent_data>>& datas)
+		: base_agent_interface(index), base_agent(index, *std::get<0>(datas)), data(*std::get<3>(datas))
+	{}
+};
+
+TEST(BaseAgentContainerTest, Instantiation)
+{
+	const generic_agent_and_data_container<base_agent> base_container(std::make_unique<base_agent_data>());
+
+	// Instantiate diffusion container
+	{
+		auto base_data = std::make_unique<physicore::base_agent_data>();
+		auto diffusion_data = std::make_unique<diffusion_agent_data>(*base_data);
+
+		const generic_agent_and_data_container<base_agent, diffusion_agent> container(std::move(base_data),
+																					  std::move(diffusion_data));
+	}
+
+	// Instantiate mechanics container
+	{
+		auto base_data = std::make_unique<physicore::base_agent_data>();
+		auto mechanics_data = std::make_unique<mechanics_agent_data>(*base_data);
+
+		const generic_agent_and_data_container<base_agent, mechanics_agent> container(std::move(base_data),
+																					  std::move(mechanics_data));
+	}
+
+	// Instantiate big (union of diffusion and mechanics) container
+	{
+		auto base_data = std::make_unique<physicore::base_agent_data>();
+		auto diffusion_data = std::make_unique<diffusion_agent_data>(*base_data);
+		auto mechanics_data = std::make_unique<mechanics_agent_data>(*base_data);
+		auto big_data = std::make_unique<big_agent_data>(*base_data, *diffusion_data, *mechanics_data);
+
+		generic_agent_and_data_container<base_agent, diffusion_agent, mechanics_agent, big_agent> container(
+			std::move(base_data), std::move(diffusion_data), std::move(mechanics_data), std::move(big_data));
+
+		// Can be down-casted
+		generic_agent_interface_container<diffusion_agent_interface>& diffusion_container = container;
+		generic_agent_interface_container<mechanics_agent_interface>& mechanics_container = container;
+
+		// Data can be accessed
+		{
+			physicore::generic_agent_solver<diffusion_agent> diff_accessor;
+			const diffusion_agent_data& d_accessed = diff_accessor.retrieve_agent_data(container);
+			(void)d_accessed;
+
+			physicore::generic_agent_solver<mechanics_agent> mech_accessor;
+			const mechanics_agent_data& m_accessed = mech_accessor.retrieve_agent_data(container);
+			(void)m_accessed;
+		}
+
+		mechanics_container.create();
+		ASSERT_EQ(diffusion_container.size(), 1);
+		ASSERT_EQ(mechanics_container.size(), 1);
+
+		diffusion_container.create();
+		ASSERT_EQ(diffusion_container.size(), 2);
+		ASSERT_EQ(mechanics_container.size(), 2);
+	}
+}
